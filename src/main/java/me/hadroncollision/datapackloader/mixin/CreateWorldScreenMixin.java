352faee8fbaefded9a1ed7c9253ca21cb573727e
HadronCollision <102247412+HadronCollision@users.noreply.github.com>
2023-12-07 16:35:12 +0500

    Update CreateWorldScreenMixin.java

--- src/main/java/me/hadroncollision/datapackloader/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/hadroncollision/datapackloader/mixin/CreateWorldScreenMixin.java
@@ -3,6 +3,7 @@ package me.hadroncollision.datapackloader.mixin;
 import me.hadroncollision.datapackloader.DataPackLoader;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.resource.*;
+import net.minecraft.util.path.SymlinkFinder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -12,13 +13,14 @@ import java.nio.file.Path;
 @Mixin(value = CreateWorldScreen.class, priority = 1001)
 public class CreateWorldScreenMixin {
 
-    @Redirect(method = "getScannedPack", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/VanillaDataPackProvider;createManager(Ljava/nio/file/Path;)Lnet/minecraft/resource/ResourcePackManager;"))
-    private ResourcePackManager getScannedPacks(Path dataPacksPath) {
-        return new ResourcePackManager(new VanillaDataPackProvider(), new FileResourcePackProvider(dataPacksPath, ResourceType.SERVER_DATA, ResourcePackSource.WORLD),
+    @Redirect(method = "getScannedPack", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/VanillaDataPackProvider;createManager(Ljava/nio/file/Path;Lnet/minecraft/util/path/SymlinkFinder;)Lnet/minecraft/resource/ResourcePackManager;"))
+    private ResourcePackManager getScannedPacks(Path dataPacksPath, SymlinkFinder symlinkFinder) {
+        return new ResourcePackManager(new VanillaDataPackProvider(symlinkFinder), new FileResourcePackProvider(dataPacksPath, ResourceType.SERVER_DATA, ResourcePackSource.WORLD, symlinkFinder),
                 new FileResourcePackProvider(
                         DataPackLoader.DATAPACKS_PATH,
                         ResourceType.SERVER_DATA,
-                        DataPackLoader.RESOURCE_PACK_SOURCE
+                        DataPackLoader.RESOURCE_PACK_SOURCE,
+                        symlinkFinder
                 ));
     }
 }
